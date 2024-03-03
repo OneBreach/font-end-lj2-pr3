a141dccd6b9d9444e64ff9a69ed6d25316e04bb5
MilanMaPangPang <120579337+MilanMaPangPang@users.noreply.github.com>
2024-03-03 12:33:19 +0100

    enter popup

--- src/components/Graphs/Graphs.tsx
+++ src/components/Graphs/Graphs.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Graphs.css';
 
 const Graphs: React.FC = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
-    <header className="Graphs-header">
+    <div className="Graphs-container">
+      {showPopup && (
+        <div className="popup-overlay">
+          <div className="popup">
+            <div className="popup-content">
+              <p className="welcome-text">Welkom bij Crypto Checker! Om toegang te krijgen tot alle functies en gedetailleerde informatie over cryptocurrencies, kun je doorgaan als gast en direct beginnen met browsen. Klik op 'Doorgaan als gast' om door te gaan naar de website en de huidige crypto-data te bekijken.</p>
+              <p className="gast-button" onClick={handleClosePopup}>Doorgaan als gast</p>
+            </div>
+          </div>
+        </div>
+      )}
+      <header className="Graphs-header">
         <div>
-      <div className="square"></div>
-      <div className="square"></div>
-      </div>
-      <div className="line"></div>
-      <div className="top"></div>
-    </header>
+          <div className="square"></div>
+          <div className="square"></div>
+        </div>
+        <div className="line"></div>
+        <div className="top"></div>
+      </header>
+    </div>
   );
 };
 
